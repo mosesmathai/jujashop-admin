24861f3071a4eaf2a3bb6d4397c7515aca537d13
unknown <mosesmathai9@gmail.com>
2023-10-21 12:26:32 +0300

    stats

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
+import HomeStats from "@/components/HomeStats";
 import Layout from "@/components/Layout";
 import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const {data: session} = useSession()
+  const {data: session} = useSession();
   return (
     <Layout>
-      Hello&#44; {session?.user?.name}&#128521;
+      <div>Hello&#44; {session?.user?.name}&#128521;</div>
+      <HomeStats />
     </Layout>
   )
 }
